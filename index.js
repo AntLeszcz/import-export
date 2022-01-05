dae2040997e74ebdbd648c47dc65351c868efc06
AntLeszcz <antek.leszczynski@gmail.com>
2022-01-05 15:33:08 +0100

    commit

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var app = (0, _express2.default)();
 
 app.get('/', function (req, res) {
-    res.send(`<iframe width="420" height="315"
-    src="https://youtu.be/PLiKpF-tLE0">
-    </iframe>`);
+    res.send(`<iframe width="560" height="315" src="https://www.youtube.com/embed/PLiKpF-tLE0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
 });
 
 app.listen(process.env.PORT || 3000, function () {
